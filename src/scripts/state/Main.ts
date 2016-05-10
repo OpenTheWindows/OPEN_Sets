5e48a3a692e4955df497537234a007002e4a9f74
sumarsky <i.dimcevski@gmail.com>
2016-05-10 10:59:50 +0200

    images resize + start screen

--- src/scripts/state/Main.ts
+++ src/scripts/state/Main.ts
@@ -1,9 +1,7 @@
 module OPENSets.State {
   export class Main extends Phaser.State {
-
     create() {
-      var thing:String = 'code !';
-      this.add.text(10, 10, `Let's ${thing}`, { font: '65px Arial' });
+      this.game.state.start('start');
     }
   }
 }
