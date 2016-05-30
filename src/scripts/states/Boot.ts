efcb40775b8bf336b08bd65a2ed57a5085d8c245
Eljesa Zekjiri <ezekjiri@endava.net>
2016-05-30 14:42:19 +0200

    minor changes according to ts lint deffintions

--- src/scripts/states/Boot.ts
+++ src/scripts/states/Boot.ts
@@ -1,41 +1,46 @@
 module OPENSets.State {
-    export class Boot extends Phaser.State {
-        preload(): void {
-            //Load pairs from json
-            this.load.text('pairs', 'configuration/pairs.json');
-            
-            this.load.image('preload-bar', 'assets/images/preloader.gif');
+  export class Boot extends Phaser.State {
+    preload(): void {
+      // Load pairs from json
+      this.load.text('pairs', 'configuration/pairs.json');
 
-            this.load.image('sets-logo', 'assets/images/sets-logo.png');
-            this.load.spritesheet('play-button', 'assets/images/play-button.png', 160, 160);
-            this.load.image('frame', 'assets/images/frame.png');
-            this.load.spritesheet('next-button', 'assets/images/next-button.png', 254, 141);
-            this.load.image('wrong', 'assets/images/wrong.png');
+      this.load.image('preload-bar', 'assets/images/preloader.gif')
+      this.load.image('sets-logo', 'assets/images/sets-logo.png');
+      this.load.spritesheet('play-button', 'assets/images/play-button.png', 160, 160);
+      this.load.image('frame', 'assets/images/frame.png');
+      this.load.spritesheet('next-button', 'assets/images/next-button.png', 254, 141);
+      this.load.image('wrong', 'assets/images/wrong.png');
 
-            // Load animations
-            this.load.atlasJSONHash(
-                'happy-animation',
-                'assets/animations/happy-animation.png',
-                'assets/animations/happy-animation.json');
+      // Load animations
+      this.load.atlasJSONHash(
+        'happy-animation',
+        'assets/animations/happy-animation.png',
+        'assets/animations/happy-animation.json');
 
-            // Load sounds
-            this.load.audio('audio-transition', 'assets/sounds/audio-transition.wav');
-            this.load.audio('audio-right-option', 'assets/sounds/audio-right-option.mp3');
-            this.load.audio('audio-wrong-option', 'assets/sounds/audio-wrong-option.wav');
-        }
+      // Load animations
+      this.load.atlasJSONHash(
+        'monkey-animation',
+        'assets/animations/monkey-animation.png',
+        'assets/animations/monkey-animation.json');
 
-        create(): void {
-            this.game.stage.backgroundColor = 0xFFFFFF;
-
-            this.input.maxPointers = 1;
-            this.stage.disableVisibilityChange = true;
-
-            // Responsive Design
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.pageAlignHorizontally = false;
-
-            this.game.state.start('preload');
-        }
+      // Load sounds
+      this.load.audio('audio-transition', 'assets/sounds/audio-transition.wav');
+      this.load.audio('audio-right-option', 'assets/sounds/audio-right-option.mp3');
+      this.load.audio('audio-wrong-option', 'assets/sounds/audio-wrong-option.wav');
     }
+
+    create(): void {
+      this.game.stage.backgroundColor = 0xFFFFFF;
+
+      this.input.maxPointers = 1;
+      this.stage.disableVisibilityChange = true;
+
+      // Responsive Design
+      this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      this.scale.pageAlignHorizontally = false;
+
+      this.game.state.start('preload');
+    }
+  }
 }
 
--- src/scripts/states/Preload.ts
+++ src/scripts/states/Preload.ts
@@ -1,19 +1,19 @@
 module OPENSets.State {
-    export class Preload extends Phaser.State {
+  export class Preload extends Phaser.State {
 
-        preload(): void {
-            let preloadBar: Phaser.Sprite = this.add.sprite(290, 290, 'preload-bar');
-            this.load.setPreloadSprite(preloadBar);
-            
-            let jsonObject = JSON.parse(this.game.cache.getText('pairs'));
-            for (var item of jsonObject.pairs) {
-                this.load.image(item.itemOne, item.pathOne);
-                this.load.image(item.itemTwo, item.pathTwo);
-            }
-        }
+    preload(): void {
+      let preloadBar: Phaser.Sprite = this.add.sprite(290, 290, 'preload-bar');
+      this.load.setPreloadSprite(preloadBar);
 
-        create(): void {
-            this.game.state.start('start');
-        }
+      let jsonObject = JSON.parse(this.game.cache.getText('pairs'));
+      for (let item of jsonObject.pairs) {
+        this.load.image(item.itemOne, item.pathOne);
+        this.load.image(item.itemTwo, item.pathTwo);
+      }
     }
+
+    create(): void {
+      this.game.state.start('start');
+    }
+  }
 }
