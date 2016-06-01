3c387445655eefd743697758b4bf242d5ffcfec4
unknown <vgrozdanovski@EN310065.endava.net>
2016-06-01 16:46:03 +0200

    Test for RandomizeGameModelService added.

---
+++ src/tests/RandomizeGameModelService.spec.ts
@@ -0,0 +1,26 @@
+describe('RandomizeGameModelServiceTests', () => {
+  let randomizeGameModelService : OPENSets.Services.RandomizeGameModelService;
+
+  beforeEach(() => {
+    randomizeGameModelService = new OPENSets.Services.RandomizeGameModelService();
+  });
+
+  it('givenRandomizeGameModelService_whenGameModelIsRandomized_outputShouldContainTreeDifferentOptions', () => {
+    // Arrange
+    let gameState = OPENSets.Helpers.GameState.getInstance();
+    gameState.randomizedPairs = OPENSets.TestsHelpers.Helpers.createPairsForTesting();
+    var gameModel = new OPENSets.Models.GameModel(gameState.randomizedPairs[0]);
+
+    // Act
+    let result = randomizeGameModelService.randomize(gameModel);
+
+    // Assert
+    expect(result.quest).toEqual(gameState.randomizedPairs[0]);
+    expect(result.question).not.toEqual(result.rightAnswer.name);
+    expect(result.question).not.toEqual(result.wrongAnswer1.name);
+    expect(result.question).not.toEqual(result.wrongAnswer2.name);
+    expect(result.rightAnswer.name).not.toEqual(result.wrongAnswer1.name);
+    expect(result.rightAnswer.name).not.toEqual(result.wrongAnswer2.name);
+    expect(result.wrongAnswer1.name).not.toEqual(result.wrongAnswer2.name);
+  });
+});
