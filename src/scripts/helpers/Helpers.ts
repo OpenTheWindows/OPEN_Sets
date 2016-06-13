8395413fefe4fba429d4a3dce8f9c3cae5b11f97
Aleksandra Despotovska <adespotovska@endava.net>
2016-06-13 10:56:12 +0200

    fixed linting issues

--- src/scripts/helpers/Helpers.ts
+++ src/scripts/helpers/Helpers.ts
@@ -1,15 +1,15 @@
 module OPENSets.Helpers {
-    export class Helpers {
-        public static shuffleArray<T>(elements : Array<T>) : Array<T> {
-            let newArray = elements; 
-            var j, x, i;
-            for (i = newArray.length; i; i -= 1) {
-                j = Math.floor(Math.random() * i);
-                x = newArray[i - 1];
-                newArray[i - 1] = newArray[j];
-                newArray[j] = x;
-            }
-            return newArray;
-        }
+  export class Helpers {
+    public static shuffleArray<T>(elements: Array<T>): Array<T> {
+      let newArray: T[] = elements;
+      let j: number, x: T, i: number;
+      for (i = newArray.length; i; i -= 1) {
+        j = Math.floor(Math.random() * i);
+        x = newArray[i - 1];
+        newArray[i - 1] = newArray[j];
+        newArray[j] = x;
+      }
+      return newArray;
     }
+  }
 }
--- src/scripts/models/GameModel.ts
+++ src/scripts/models/GameModel.ts
@@ -1,16 +1,16 @@
 module OPENSets.Models {
   export class GameModel {
-    public quest : Models.Pair;
-    public question : string;
-    public rightAnswer = new Models.Option("", true);
-    public wrongAnswer1 = new Models.Option("", false);
-    public wrongAnswer2 = new Models.Option("", false);
+    public quest: Models.Pair;
+    public question: string;
+    public rightAnswer: Models.Option = new Models.Option('', true);
+    public wrongAnswer1: Models.Option = new Models.Option('', false);
+    public wrongAnswer2: Models.Option = new Models.Option('', false);
 
-    constructor(quest : Models.Pair) {
+    constructor(quest: Models.Pair) {
       this.quest = quest;
     }
 
-    public getRandomOptions() : Models.Option[] {
+    public getRandomOptions(): Models.Option[] {
       return Helpers.Helpers.shuffleArray([this.rightAnswer, this.wrongAnswer1, this.wrongAnswer2]);
     }
   }
