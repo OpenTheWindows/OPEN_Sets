e1c84982b69e864983f1dadd2e3a96f720dbfb0f
Aleksandra Despotovska <adespotovska@endava.net>
2016-06-27 16:48:30 +0200

    fixed bug: same order of pairs after starting a new game

--- src/scripts/helpers/GameState.ts
+++ src/scripts/helpers/GameState.ts
@@ -2,13 +2,11 @@ module OPENSets.Helpers {
   export class GameState {
     private static _instance: GameState = new GameState();
 
-    public pairs: Array<Models.Pair>;
-    public animations: Array<Models.AnimationModel>;
     public wrongTriesTreshold: number;
-    public pairsImagesPrefix: string = 'assets/pairs/';
-    public animationsPrefix: string = 'assets/animations/';
-    public imageSuffix: string = '.png';
-    public jsonSuffix: string = '.json';
+
+    public pairs: Array<Models.Pair>;
+    public animations: Array<Models.Animation>;
+    public shuffledAnimations: Array<Models.Animation>;
 
     public static getInstance(): GameState {
       return GameState._instance;
@@ -21,8 +19,25 @@ module OPENSets.Helpers {
 
       GameState._instance = this;
       this.pairs = new Array<Models.Pair>();
-      this.animations = new Array<Models.AnimationModel>();
-      this.wrongTriesTreshold = 6;
+      this.animations = new Array<Models.Animation>();
+      this.shuffledAnimations = new Array<Models.Animation>();
+    }
+
+    newGame(): void {
+      this.pairs = Helpers.shuffleArray(this.pairs);
+      this.shuffledAnimations = Helpers.shuffleArray(this.animations);
+
+      let animationsLenght: number = this.shuffledAnimations.length;
+      let pairsLenght: number = this.pairs.length;
+
+      while (animationsLenght < pairsLenght) {
+        animationsLenght += animationsLenght;
+        this.shuffledAnimations = this.animations.concat(this.shuffledAnimations);
+      }
+    }
+
+    getAnimations(): Array<Models.Animation> {
+      return this.shuffledAnimations;
     }
   }
 }
