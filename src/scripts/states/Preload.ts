c34e84938e70adf71366f6d4e52d40ae2df20356
Aleksandra Despotovska <adespotovska@endava.net>
2016-06-29 13:10:31 +0200

    refactored the preload state

--- src/scripts/states/Preload.ts
+++ src/scripts/states/Preload.ts
@@ -1,6 +1,6 @@
 module OPENSets.State {
   export class Preload extends Phaser.State {
-    private gameState: Helpers.GameState;
+    private preloadService: Services.PreloadManagementService;
 
     private pairsImagesPrefix: string;
     private animationsPrefix: string;
@@ -9,21 +9,32 @@ module OPENSets.State {
 
     constructor() {
       super();
-      this.gameState = Helpers.GameState.getInstance();
+      this.preloadService = new Services.PreloadManagementService();
     }
 
     preload(): void {
       let preloadBar: Phaser.Sprite = this.add.sprite(this.game.world.centerX - 110, this.game.world.centerY, 'loader');
       this.load.setPreloadSprite(preloadBar);
 
+      this.loadCongif();
+      this.loadPairs();
+      this.loadAnimations();
+    }
+
+    create(): void {
+      this.game.state.start('start');
+    }
+
+    loadCongif(): void {
       let config: any = JSON.parse(this.game.cache.getText('config'));
 
-      this.gameState.wrongTriesTreshold = config.wrongTriesTreshold;
+      this.preloadService.setWrongTriesTreshold(config.wrongTriesTreshold);
       this.pairsImagesPrefix = config.pairsPath;
       this.animationsPrefix = config.animationsPath;
+    }
 
+    loadPairs(): void {
       let pairs: Array<Models.Pair> = JSON.parse(this.game.cache.getText('pairs'));
-      let animations: Array<Models.Animation> = JSON.parse(this.game.cache.getText('happy-animations'));
 
       for (let pair of pairs) {
         this.load.image(
@@ -33,24 +44,22 @@ module OPENSets.State {
         this.load.image(
           pair.itemTwo,
           this.pairsImagesPrefix + pair.itemTwo + this.imageSuffix);
-
-        this.gameState.pairs.push(pair);
       }
+
+      this.preloadService.setPairs(pairs);
+    }
+
+    loadAnimations(): void {
+      let animations: Array<Models.Animation> = JSON.parse(this.game.cache.getText('happy-animations'));
 
       for (let animation of animations) {
         this.load.atlasJSONHash(
           animation.name,
           this.animationsPrefix + animation.name + this.imageSuffix,
           this.animationsPrefix + animation.name + this.jsonSuffix);
-
-        this.gameState.animations.push(animation);
       }
 
-      this.gameState.newGame();
-    }
-
-    create(): void {
-      this.game.state.start('start');
+      this.preloadService.setAnimations(animations);
     }
   }
 }
