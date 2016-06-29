daf67b7ab888d85559bc0be7ab2c748e92b4e2d9
Aleksandra Despotovska <adespotovska@endava.net>
2016-06-29 16:00:59 +0200

    added tests and createAnimationsForTesting helper method

---
+++ src/tests/MainGameManagementService.spec.ts
@@ -0,0 +1,30 @@
+namespace OPENSets.Tests {
+  describe('MainGameManagementServiceTests', () => {
+    let mainGameService: Services.MainGameManagementService;
+    let gameState: Helpers.GameState;
+    let treshold: number = 6;
+
+    beforeEach(() => {
+      gameState = Helpers.GameState.getInstance();
+      gameState.setWrongTriesTreshold(treshold);
+      mainGameService = new Services.MainGameManagementService();
+    });
+
+    it('getGameModelForThisIteration_shouldResetCounterAndReturnGameModel', () => {
+      // Arrange
+      spyOn(gameState, 'getCurrentPair').and.callFake(() => {
+        return TestsHelpers.Helpers.createPairsForTesting()[0];
+      });
+      spyOn(gameState, 'getAllPairs').and.callFake(() => {
+        return TestsHelpers.Helpers.createPairsForTesting();
+      });
+
+      // Act
+      let gameModel: Models.GameModel = mainGameService.getGameModelForCurrentIteration();
+
+      // Assert
+      expect(gameModel).toBeDefined();
+      expect(mainGameService.getTries()).toBe(0);
+    });
+  });
+}
